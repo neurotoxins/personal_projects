01a8f1e518598d066b181126f7ec3041933691f8
cravisteja <cravisteja@gmail.com>
2017-04-13 00:35:05 -0400

    final commit

---
+++ Project_1/movieClass.py
@@ -0,0 +1,7 @@
+class Movies:
+
+    def __init__(self,titles,summary,image,url):
+        self.title=titles
+        self.storyline = summary
+        self.poster_image_url=image
+        self.trailer_youtube_url=url
